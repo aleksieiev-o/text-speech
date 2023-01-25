bb79cf738ae15c9e01008da3e5d782b277fcc2a6
Alexander Alexeev <alekseev@trueconf.ru>
2023-01-25 18:26:47 +0100

    add react-bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'styles/index.scss';
+import './styles/index.scss';
 import AppComponent from './App.component';
 import reportWebVitals from './reportWebVitals';
 
