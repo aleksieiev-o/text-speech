3f8270f66342135f16d7ef3c7c050bb77b4a38e2
Alexander Alexeev <alekseev@trueconf.ru>
2023-02-05 20:57:29 +0100

    add remove collections confirm modals

--- src/components/ListHeader/index.tsx
+++ src/components/ListHeader/index.tsx
@@ -1,9 +1,10 @@
 import React, { FC, ReactElement } from 'react';
 import { useCollectionsStore } from '../../store/hooks';
 import { observer } from 'mobx-react-lite';
-import { Button, Icon, Stack } from '@chakra-ui/react';
+import { Button, Icon, Stack, useDisclosure } from '@chakra-ui/react';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ActionConfirmationModal, { ActionConfirmationModalType } from '../ActionConfirmation.modal';
 
 interface Props {
   onOpen: () => void;
@@ -14,32 +15,53 @@ interface Props {
 
 const ListHeader: FC<Props> = observer((props): ReactElement => {
   const {createButtonTitle, removeButtonTitle, onOpen} = props;
+  const { isOpen, onOpen: onOpenConfirmModal, onClose } = useDisclosure();
   const collectionsStore = useCollectionsStore();
 
-  return (
-    <Stack
-      as={'section'}
-      direction={'row'}
-      w={'full'}
-      alignItems={'center'}
-      justifyContent={'flex-end'}
-      mb={4}
-      p={4}
-      boxShadow={'md'}>
-      <Button
-        onClick={onOpen}
-        colorScheme={'facebook'}
-        leftIcon={<Icon as={AddIcon}/>}>
-        {createButtonTitle}
-      </Button>
+  const removeAllCollectionsHandler = async () => {
+    await collectionsStore.removeAllCollections();
+    onClose();
+  };
 
-      <Button
-        onClick={() => collectionsStore.removeAllCollections()}
-        colorScheme={'red'}
-        leftIcon={<Icon as={DeleteIcon}/>}>
-        {removeButtonTitle}
-      </Button>
-    </Stack>
+  return (
+    <>
+      <Stack
+        as={'section'}
+        direction={'row'}
+        w={'full'}
+        alignItems={'center'}
+        justifyContent={'flex-end'}
+        mb={4}
+        p={4}
+        boxShadow={'md'}>
+        <Button
+          onClick={onOpen}
+          colorScheme={'facebook'}
+          leftIcon={<Icon as={AddIcon}/>}>
+          {createButtonTitle}
+        </Button>
+
+        <Button
+          onClick={onOpenConfirmModal}
+          colorScheme={'red'}
+          leftIcon={<Icon as={DeleteIcon}/>}>
+          {removeButtonTitle}
+        </Button>
+      </Stack>
+
+      {
+        isOpen &&
+        <ActionConfirmationModal
+          actionHandler={removeAllCollectionsHandler}
+          isOpen={isOpen}
+          onClose={onClose}
+          modalType={ActionConfirmationModalType.DANGER}
+          modalTitle={'Remove all collections confirmation'}
+          modalBodyDescription={'You are about to remove all collection.'}
+          modalBodyQuestion={'Are you cure?'}
+          buttonText={'Remove'}/>
+      }
+    </>
   );
 });
 
--- src/views/CollectionsList/index.tsx
+++ src/views/CollectionsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import ListHeader from '../../components/ListHeader';
 import Header from '../../components/Header';
 import { useCollectionsStore } from '../../store/hooks';
@@ -9,10 +9,28 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import CreateCollectionModal from '../../components/ListHeader/CreateCollection.modal';
+import ActionConfirmationModal, { ActionConfirmationModalType } from '../../components/ActionConfirmation.modal';
 
 const CollectionsList: FC = observer((): ReactElement => {
   const collectionsStore = useCollectionsStore();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [tempCollection, setTempCollection] = useState<Collection>({} as Collection);
+  const { isOpen: isOpenCreateCollectionModal, onOpen: onOpenCreateCollectionModal, onClose: onCloseCreateCollectionModal } = useDisclosure();
+  const { isOpen: isOpenCRemoveCollectionModal, onOpen: onOpenCRemoveCollectionModal, onClose: onCloseRemoveCCollectionModal } = useDisclosure();
+
+  const prepareToRemoveCollection = (collection: Collection) => {
+    setTempCollection(collection);
+    onOpenCRemoveCollectionModal();
+  };
+
+  const closeRemoveCollectionModalHandler = () => {
+    onCloseRemoveCCollectionModal();
+    setTempCollection({} as Collection);
+  };
+
+  const actionRemoveCollectionModalHandler = async () => {
+    await collectionsStore.removeCollection(tempCollection.id);
+    closeRemoveCollectionModalHandler();
+  };
 
   return (
     <>
@@ -26,7 +44,7 @@ const CollectionsList: FC = observer((): ReactElement => {
       h={'full'}
       overflow={'hidden'}>
         <ListHeader
-        onOpen={onOpen}
+        onOpen={onOpenCreateCollectionModal}
         removeButtonHandler={collectionsStore.removeAllCollections}
         createButtonTitle={'Create collection'}
         removeButtonTitle={'Remove all collections'}/>
@@ -67,7 +85,7 @@ const CollectionsList: FC = observer((): ReactElement => {
                           </Button>
 
                           <Button
-                          onClick={() => collectionsStore.removeCollection(collection.id)}
+                          onClick={() => prepareToRemoveCollection(collection)}
                           colorScheme={'red'}
                           leftIcon={<Icon as={DeleteIcon}/>}>
                             Remove
@@ -91,7 +109,7 @@ const CollectionsList: FC = observer((): ReactElement => {
               </Text>
 
               <Button
-              onClick={onOpen}
+              onClick={onOpenCreateCollectionModal}
               colorScheme={'facebook'}
               leftIcon={<Icon as={AddIcon}/>}>
                 Create collection
@@ -100,7 +118,20 @@ const CollectionsList: FC = observer((): ReactElement => {
         }
       </Stack>
 
-      {isOpen && <CreateCollectionModal isOpen={isOpen} onClose={onClose}/>}
+      {isOpenCreateCollectionModal && <CreateCollectionModal isOpen={isOpenCreateCollectionModal} onClose={onCloseCreateCollectionModal}/>}
+
+      {
+        isOpenCRemoveCollectionModal &&
+        <ActionConfirmationModal
+          actionHandler={actionRemoveCollectionModalHandler}
+          isOpen={isOpenCRemoveCollectionModal}
+          onClose={closeRemoveCollectionModalHandler}
+          modalType={ActionConfirmationModalType.DANGER}
+          modalTitle={'Remove collection confirmation'}
+          modalBodyDescription={`You are about to remove collection ${tempCollection.title}.`}
+          modalBodyQuestion={'Are you cure?'}
+          buttonText={'Remove'}/>
+      }
     </>
   );
 });
