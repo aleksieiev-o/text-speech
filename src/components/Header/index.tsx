5adc008674da91fc60e5d88a4b03242c4087b438
Alexander Alexeev <alekseev@trueconf.ru>
2023-02-05 20:33:35 +0100

    add ActionConfirmationModal and add logout confirm modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,13 @@ import { useAuthorizationStore } from '../../store/hooks';
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
 import { PublicRoutes } from '../../Router';
-import { Button, Heading, Icon, Stack } from '@chakra-ui/react';
+import { Button, Heading, Icon, Stack, useDisclosure } from '@chakra-ui/react';
 import LogoutIcon from '@mui/icons-material/Logout';
+import ActionConfirmationModal, { ActionConfirmationModalType } from '../ActionConfirmation.modal';
 
 const Header: FC = observer((): ReactElement => {
   const authorizationStore = useAuthorizationStore();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const navigate = useNavigate();
 
   const logoutHandler = async () => {
@@ -21,21 +23,36 @@ const Header: FC = observer((): ReactElement => {
   };
 
   return (
-    <Stack
-    as={'header'}
-    direction={'row'}
-    w={'full'}
-    alignItems={'center'}
-    justifyContent={'space-between'}
-    boxShadow={'md'}
-    p={4}>
-      <Heading as={'h4'} color={'facebook.600'}>{APP_NAME}</Heading>
+    <>
+      <Stack
+        as={'header'}
+        direction={'row'}
+        w={'full'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        boxShadow={'md'}
+        p={4}>
+        <Heading as={'h4'} color={'facebook.600'}>{APP_NAME}</Heading>
+
+        {
+          authorizationStore.isAuth &&
+          <Button onClick={onOpen} colorScheme={'orange'} rightIcon={<Icon as={LogoutIcon}/>}>Logout</Button>
+        }
+      </Stack>
 
       {
-        authorizationStore.isAuth &&
-        <Button onClick={logoutHandler} colorScheme={'orange'} rightIcon={<Icon as={LogoutIcon}/>}>Logout</Button>
+        isOpen &&
+        <ActionConfirmationModal
+          actionHandler={logoutHandler}
+          isOpen={isOpen}
+          onClose={onClose}
+          modalType={ActionConfirmationModalType.WARNING}
+          modalTitle={'Logout confirmation'}
+          modalBodyDescription={'You are about to log out.'}
+          modalBodyQuestion={'Are you cure?'}
+          buttonText={'Logout'}/>
       }
-    </Stack>
+    </>
   );
 });
 
