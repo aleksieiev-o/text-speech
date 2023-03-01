cf43bfa78f183147e465267132cd235a8ed56545
Alexander Alexeev <alekseev@trueconf.ru>
2023-03-01 22:15:15 +0100

    rename project

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = 'Text speech';
+export const APP_NAME = 'Text to speech';
 
 export const BASE_URL = process.env.NODE_ENV === 'production' ? `http://localhost:3000` : `http://localhost:3000`;
